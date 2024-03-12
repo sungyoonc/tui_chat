127612ca7c044f70e02836971f9bd1c1e192f3ad
random6 <random6.xyz@gmail.com>
2024-03-12 22:03:27 +0900

    add salt encryption in login

--- server/src/routes/handlers/login.rs
+++ server/src/routes/handlers/login.rs
@@ -1,6 +1,6 @@
 use std::convert::Infallible;
 use crate::routes::*;
-use mysql::{params, prelude::Queryable, Pool, Row};
+use mysql::{params, prelude::Queryable, Pool, Row, Value};
 use std::env;
 use crate::utils;
 use rand_core::{RngCore, OsRng};
@@ -23,25 +23,29 @@ impl Db {
     }    
 }
 
-
 pub async fn login(json_data: LoginData) -> Result<impl warp::Reply, Infallible> {
-    let hashed_id = utils::hash_from_string(json_data.clone().id);
-    let hashed_pw = utils::hash_from_string(json_data.pw);
+    let id = json_data.clone().id;
+    let pw = json_data.pw;
 
     let mut conn = Db::new().pool.get_conn().unwrap();
-    let result: Vec<Row> = conn.exec("SELECT * FROM login WHERE id = :id AND pw= :pw", params! {"id" => hashed_id.clone(), "pw" => hashed_pw}).unwrap();
+    let result: Vec<Row> = conn.exec("SELECT salt, pw FROM login WHERE id = :id", params! {"id" => id.clone()}).unwrap();
+    if result.len() == 0 {
+        return Ok(warp::reply::with_status("login error", StatusCode::UNAUTHORIZED))
+    }
+    let (salt, db_pw): (String, String) = mysql::from_row(result[0].clone());
 
-    if result.len() != 1 {
+    let hashed_pw = utils::hash_from_string(format!("{}{}", pw, salt));
+    if hashed_pw != db_pw {
         return Ok(warp::reply::with_status("login error", StatusCode::UNAUTHORIZED))
     }
 
     let mut key = OsRng.next_u64().to_le_bytes().to_vec();
-    let mut session_source = hashed_id.clone().into_bytes();
+    let mut session_source = id.clone().into_bytes();
     session_source.append(&mut key);
     let session = utils::hash_from_u8(session_source);
     let expire_time = Utc::now() + TimeDelta::try_hours(SESSION_EXPIRE_HOUR).unwrap();
     let expire = format!("{}-{}-{} {}:{}:{}.{}", expire_time.year(), expire_time.month(), expire_time.day(), expire_time.hour(), expire_time.minute(), expire_time.second(), (expire_time.nanosecond() as i64) % 10000000);
-    let _result: Vec<Row> = conn.exec("INSERT INTO session (id, session, expire) VALUES (:id, :session, :expire)", params! {"id" => hashed_id, "session" => session.clone(), "expire" => expire}).unwrap();
+    let _result: Vec<Row> = conn.exec("INSERT INTO session (id, session, expire) VALUES (:id, :session, :expire)", params! {"id" => id, "session" => session.clone(), "expire" => expire}).unwrap();
 
     return Ok(warp::reply::with_status("login success", StatusCode::OK))
 }
